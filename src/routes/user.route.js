8979a2a604f13e069eb973f072acdfb3fb4b0a74
Karan Mharse <krnmharse06@gmail.com>
2024-01-07 09:47:53 -0800

    user logout route

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const {userRegister,userLogin} = require('../controllers/user.controller')
+const {userRegister,userLogin,userLogout} = require('../controllers/user.controller')
 const upload = require("../middleware/multer.middleware")
+const {verifyUser} = require('../middleware/verifyUser.middleware')
 
 
 router.route('/register').post(
     upload.single('avatar'),userRegister
 )
 router.route('/login').post(userLogin)
+router.route('/logout').post(verifyUser,userLogout)
 
 
 module.exports = router
